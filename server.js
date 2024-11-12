63a2cf3a09eb39ea5f55f35c2b2a7da6b26ff197
CuongLeManh2910 <lecuong291005@gmail.com>
2024-11-12 10:56:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -23,3 +23,55 @@ app.get('/',  async (req,res) => {
 
     res.send(phones);
 })
+
+
+app.get('/add_phone', async (req, res) =>{
+    await mongoose.connect(uri);
+
+    let phone = {
+        ten: 'Nokia 1280',
+        hang: 'Nokia',
+        namSX: 2012,
+        gia: 500,
+    }
+
+    let kq = await phoneModel.create(phone);
+    console.log(kq);
+
+    let phones = await phoneModel.find();
+
+    console.log(phones);
+
+    res.send(phones);
+})
+
+app.get('/xoa/:id', async (req, res) => {
+    await mongoose.connect(uri);
+
+    let id = req.params.id;
+
+    console.log(id);
+
+    // xu li loi khi id ko dung
+    await phoneModel.deleteOne({_id:id});
+
+    res.redirect('../')
+
+})
+
+app.get('/update/:ten', async (req, res)=> {
+    await mongoose.connect(uri);
+
+    console.log('ket noi db thanh cong');
+
+    let phoneName = req.params.ten;
+    console.log(phoneName);
+
+    let newPhoneName = phoneName + 'ban moi 2024 ';
+
+    await phoneModel.updateOne({ten: phoneName}, {ten: newPhoneName});
+
+    let phones = await phoneModel.find({});
+
+    res.send(phones);
+})
